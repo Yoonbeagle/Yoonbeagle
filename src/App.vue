df34e254ae3607d681c5a291dee9d4f8f45f220f
Yoonbeagle <sympathi33@gmail.com>
2022-05-18 22:42:26 +0900

    App.vue 수정

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,13 @@
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld'
+import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
 
   components: {
-    // HelloWorld
+    HelloWorld
   },
 
   data () {
